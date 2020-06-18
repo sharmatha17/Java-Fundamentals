941a97a0b634d6aaa5af278b0087bb86b180fe60
sharmatha17 <60655169+sharmatha17@users.noreply.github.com>
2020-06-18 12:20:17 +0530

    LanguageBasics

---
+++ LangageBasics2.java
@@ -0,0 +1,8 @@
+public class LangageBasics2 
+{
+	public static void main(String[] args)
+	{
+		System.out.print("Welcome ");
+		System.out.print(args[0]);
+	}
+}
---
+++ LanguageBasics3.java
@@ -0,0 +1,13 @@
+public class LanguageBasics3 
+{
+	public static void main(String[] args)
+	{
+		int a;
+		int b;
+		a=Integer.parseInt(args[0]);
+		b=Integer.parseInt(args[1]);
+		int c=a+b;
+		System.out.print("The sum of "+a+" and "+b+" is "+c);
+		
+	}
+}
